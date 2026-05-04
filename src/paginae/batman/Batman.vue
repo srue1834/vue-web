574fd86b24f2a4dab276aee3e4908f9785c8d107
srue1834 <53914141+srue1834@users.noreply.github.com>
2026-05-04 15:13:04 -0600

    saruelar: menu responsive con nuevo componente de toggle

--- src/paginae/batman/Batman.vue
+++ src/paginae/batman/Batman.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { House } from 'lucide-vue-next'
+import { House, Menu } from 'lucide-vue-next'
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -10,14 +10,67 @@ import {
 
 import CarrusImaginum from '@/components/CarrusImaginum.vue';
 import { scrollToSection } from '@/utils/scrollToSection';
+import { onMounted, onUnmounted, ref } from 'vue';
+import { Toggle } from '@/components/ui/toggle'
 
 const photos = ["justice", "arkham", "superman", "varios", "villana", "villano", "grupo", "robin", "anne", "joker", "resplandor", "cat", "gafas", "league", "fondoVerde"]; 
+
+interface Coordinatas {
+    x: number
+    y: number
+}
+
+const mousePositione = ref<Coordinatas>({x:0, y:0})
+
+const videreMenu = ref<boolean>(true)
+
+const handleResize = () => {
+    if(window.innerWidth <= 640) {
+        videreMenu.value = false
+    } else {
+        videreMenu.value = true
+    }
+}
+
+onMounted(() => {
+    handleResize()
+    window.addEventListener('resize', handleResize)
+})
+
+onUnmounted(() => {
+    window.removeEventListener('resize', handleResize)
+})
+
+const cumMouseMove = (e: MouseEvent) => {
+    const rect = (e.target as HTMLElement).getBoundingClientRect()
+    const centerX = rect.width / 2
+    const centerY = rect.height / 2
+
+    const mouseX = e.clientX - rect.left
+    const mouseY = e.clientY - rect.top
+    
+    mousePositione.value = {
+        x: (centerX - mouseX) * 0.1,
+        y: (centerY - mouseY) * 0.1
+    }
+}
+
+
+const cumMouseLeave = () => {
+    mousePositione.value = {x:0, y:0}
+}
 
 </script>
 
 <template>
     <div class="batman">
-        <nav class="extra-nav flex flex-col sm:flex-row justify-between px-3">
+        <Toggle
+            class="fixed top-2 right-4 bg-slate-500"
+            @click="videreMenu = !videreMenu"
+        >
+            <Menu />
+        </Toggle>
+        <nav v-if="videreMenu" class="extra-nav flex flex-col sm:flex-row justify-between px-3">
             <RouterLink to="/">
                 <House class="icon-home" />
             </RouterLink>
@@ -63,7 +116,18 @@ const photos = ["justice", "arkham", "superman", "varios", "villana", "villano",
 
         <header class="titulus">
             <h1>Batman</h1>
-            <div id="titulus-batman" class="titulus-img"></div>
+            <div 
+                id="titulus-batman" 
+                class="titulus-img"
+                @mousemove="cumMouseMove"
+                @mouseleave="cumMouseLeave"
+                :style="{
+                    backgroundPositionX: `calc(50% + ${mousePositione.x}px)`,
+                    backgroundPositionY: `calc(50% + ${mousePositione.y}px)`,
+                    transition: 'background-position 0.1s ease-out'
+                }"
+
+            ></div>
             <p>Él puede tomar la decisión que nadie más puede, la decisión correcta</p>
         </header>
 
@@ -153,9 +217,9 @@ const photos = ["justice", "arkham", "superman", "varios", "villana", "villano",
 
     .titulus-img {
         background-size: 100% 100%;
+        aspect-ratio: 8 / 7;
         background-position: center center;
         background-image: url("../imagines/batman/batman.jpg");
-        min-height: 100vh;
     }
 
     .titulus-img:hover {
@@ -164,7 +228,7 @@ const photos = ["justice", "arkham", "superman", "varios", "villana", "villano",
 
     .titulus > h1 {
         position: absolute;
-        top: 63%;
+        top: calc(100vw * 0.5);
         width: 100%;
         text-align: center;
         font-size: 5rem; 
@@ -175,7 +239,7 @@ const photos = ["justice", "arkham", "superman", "varios", "villana", "villano",
 
     .titulus > p {
         position: absolute;
-        top: 36%;
+        top: calc(100vw * 0.25);
         width: 100%;
         text-align: center;
         font-size: 2rem;
