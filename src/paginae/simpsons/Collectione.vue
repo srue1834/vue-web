2b19ba8035819ab54508264c042c9123e55d8114
srue1834 <53914141+srue1834@users.noreply.github.com>
2026-05-04 19:29:26 -0600

    saruelar: Implementacion de paginador

--- src/paginae/simpsons/Collectione.vue
+++ src/paginae/simpsons/Collectione.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 import { Card, CardContent } from '@/components/ui/card' 
-import { ref } from 'vue';
-import { characters, type Character } from './data';
+import { computed, ref } from 'vue';
+import { characters } from './data';
 
-const listaSimpsons = ref<Character[]>(characters.slice(0,6))
+
+const inPagina = 6
+const nuncPagina = ref(1)
+const totalPaginae = Math.ceil(characters.length / inPagina)
+const listaSimpsons = computed(() => {
+    const inicio = (nuncPagina.value - 1) * inPagina
+    const fin = inicio + inPagina
+    return characters.slice(inicio, fin)
+})
+
+const ireAdPaginam = (pagina:number) => {
+    if(pagina >= 1 && pagina <= totalPaginae) {
+        nuncPagina.value = pagina
+    }
+}
+
+const paginaNumeri: number[] = [ ...Array(totalPaginae)].map((_,i) => i+1)
+
 </script>
 
 <template>
@@ -27,6 +44,44 @@ const listaSimpsons = ref<Character[]>(characters.slice(0,6))
                     <h2 class="font-medium text-lg">{{ character.nomen }}</h2>
                 </CardContent>
             </Card>
+        </div>
+
+        <div class="flex items-center justify-center gap-2 mt-4">
+            <button
+                @click="ireAdPaginam(nuncPagina-1)"
+                :disabled="nuncPagina === 1"
+                :class="['px-4 py-2 rounded-md font-medium transition colors',
+                    nuncPagina === 1
+                     ? 'bg-gray-200 text-gray-400 cursor-not-allowed'
+                     : 'bg-[#ffde00] hover:bg-[#ee3133]'
+                ]"
+            >
+                Anterior
+            </button>
+
+            <button
+                v-for="pagina in paginaNumeri"
+                @click="ireAdPaginam(pagina)"
+                :class="['w-10 h-10 rounded-md font-medium transition-colors  ',
+                    nuncPagina === pagina
+                     ? 'bg-[#ee3133] text-white'
+                     : 'bg-[#ffde00] hover:bg-[#ee3133] hover:text-white'
+                 ]"
+            >
+                {{ pagina }}
+            </button>
+
+            <button
+                :disabled="nuncPagina === totalPaginae"
+                @click="ireAdPaginam(nuncPagina+1)"
+                :class="['px-4 py-2 rounded-md font-medium transition colors',
+                    nuncPagina === totalPaginae
+                     ? 'bg-gray-200 text-gray-400 cursor-not-allowed'
+                     : 'bg-[#ffde00] hover:bg-[#ee3133]'
+                ]"
+            >
+                Siguiente
+            </button>
         </div>
         
 
